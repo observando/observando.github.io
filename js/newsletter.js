c8d1b8e04dc5889da02bb859218f6cd96a70c16a
Axel Vasquez <axelv@squez.com.ar>
2017-07-06 17:33:38 -0300

    Correciones de formato
    Se cambia la tabla por una lista

--- js/newsletter.js
+++ js/newsletter.js
@@ -3,9 +3,9 @@ $(function() {
      .success(function(data) {
         var ul;
         for (var i = 0; i < data.length; i++) {
-            ul = $('<ul/>');
-            ul.append("<li>" + data[i].date + " - <a href=" + data[i].link + ">" + data[i].title + "</a></li>");
-            $('table').append(ul);
+            ul = $('<li/>');
+            ul.append("" + data[i].date + " - <a href=" + data[i].link + ">" + data[i].title + "</a></li>");
+            $('ul').append(ul);
         }
     })
     .error(function(e) { console.error(e); })
